eca2774eec41632352d1586abd4334eb0829d7ac
namenu <hw.nam@ridi.com>
2016-02-25 16:23:01 +0900

    CMS DocumentRoot 분리

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+use Ridibooks\Library\UrlHelper;
+use Ridibooks\Platform\Cms\Auth\AdminAuthService;
+
+require_once __DIR__ . '/../admin/include/bootstrap_cms.php';
+
+if (AdminAuthService::isValidLogin()) {
+	UrlHelper::redirect('/admin/welcome');
+} else {
+	UrlHelper::redirect('/admin/login');
+}
