8ded98da0b4a3b8e1027255f8146e55fc70e5c14
namenu <hw.nam@ridi.com>
2016-05-11 20:55:18 +0900

    redirect to '/welcome' instead of returning 404

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 use Ridibooks\Platform\Cms\MiniRouter;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 require_once __DIR__ . '/include/bootstrap_cms.php';
 
 if (basename($_SERVER['SCRIPT_NAME']) == basename(__FILE__)) {
 	if (!MiniRouter::selfRouting(__DIR__ . '/controls', __DIR__ . '/views')) {
-		return Response::create('Not Found', 404)->send();
+		return RedirectResponse::create('/welcome')->send();
 	}
 }
