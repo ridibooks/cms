439f2bc352dcbe264ea79a8445fa8b733eab39ec
dod <bj.kim@ridi.com>
2016-06-13 15:00:56 +0900

    로그아웃 시 불필요한 로직 제거

--- controls/logout.php
+++ controls/logout.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 $_SESSION['session_admin_id'] = null;
 $_SESSION['session_user_auth'] = null;
 $_SESSION['session_user_menu'] = null;
@@ -9,8 +11,4 @@ $_SESSION['session_user_tagid'] = null;
 //Warning: session_destroy(): Session object destruction failed
 @session_destroy();
 
-if (\Config::$ENABLE_SSL) {
-	Ridibooks\Library\UrlHelper::redirectHttps('/login');
-} else {
-	Ridibooks\Library\UrlHelper::redirect('/login');
-}
+return RedirectResponse::create('/');
