5a7162a74baa8fd7ce1c18e1b537f8e6324b0b3c
Hyunwoo Nam <hw.nam@ridi.com>
2016-06-17 23:26:24 +0900

    유저 생성

--- controls/super/user_detail.php
+++ controls/super/user_detail.php
@@ -1,5 +1,4 @@
 <?php
-use Ridibooks\Library\UrlHelper;
 use Ridibooks\Platform\Cms\Auth\AdminUserService;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -7,15 +6,13 @@ $request = Request::createFromGlobals();
 $adminUserService = new AdminUserService();
 $admin_id = $request->get("id");
 
-//유저 상세 정보
+// 유저 상세 정보
 $userDetail = $adminUserService->getAdminUser($admin_id);
-if (!$userDetail) {
-	return UrlHelper::printAlertRedirect('/super/user_list', '존재하지 않는 사용자입니다.');
-}
 
-//유저 태그 매핑 정보
+// 유저 태그 매핑 정보
 $userTag = $adminUserService->getAdminUserTag($admin_id);
-//유저 메뉴 매핑 정보
+
+// 유저 메뉴 매핑 정보
 $userMenu = $adminUserService->getAdminUserMenu($admin_id);
 
 return compact(
