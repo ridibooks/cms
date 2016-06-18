9436bc8fde245c4843819ac212e88481dea2a97f
Hyunwoo Nam <hw.nam@ridi.com>
2016-06-19 04:18:54 +0900

    CMS 모델 ActiveRecord 로 관리

--- include/bootstrap_cms.php
+++ include/bootstrap_cms.php
@@ -9,3 +9,22 @@ mb_regex_encoding("UTF-8");
 
 session_set_cookie_params(60 * 60 * 24 * 30, '/', Config::$ADMIN_DOMAIN);
 session_start();
+
+
+// PSR-4 autoload
+$autoloader = require __DIR__ . "/../vendor/autoload.php";
+
+$capsule = new Illuminate\Database\Capsule\Manager();
+
+$capsule->addConnection([
+	'driver'    => 'mysql',
+	'host'      => \Config::$DB_HOST,
+	'database'  => 'bom',
+	'username'  => \Config::$DB_USER,
+	'password'  => \Config::$DB_PASSWD,
+	'charset'   => 'utf8',
+	'collation' => 'utf8_unicode_ci',
+	'prefix'    => '',
+]);
+
+$capsule->bootEloquent();
