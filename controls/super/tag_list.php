bd552e31f1bbe15e431e32562f2e974899518ee9
Hyunwoo Nam <hw.nam@ridi.com>
2016-06-23 23:50:03 +0900

    Today's ORM  - 메뉴 위치 수정, max/increment/decrement 써봄

--- controls/super/tag_list.php
+++ controls/super/tag_list.php
@@ -1,13 +1,10 @@
 <?php
 use Ridibooks\Platform\Cms\Auth\AdminTagService;
-use Ridibooks\Platform\Cms\Auth\Dto\AdminTagDto;
 use Symfony\Component\HttpFoundation\Request;
 
 $request = Request::createFromGlobals();
-$tagService = new AdminTagService();
-$tagDto = new AdminTagDto($request);
 
-$tag_list = $tagService->getTagListWithUseCount();
+$tag_list = AdminTagService::getTagListWithUseCount();
 
 return compact(
 	'tag_list'
