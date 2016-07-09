0ce36d5b402680c45e3a8c14398df4e7c7e5018a
Hyunwoo Nam <hw.nam@ridi.com>
2016-07-09 20:58:49 +0900

    Silex Route 포팅

--- index.php
+++ index.php
@@ -1,13 +1,77 @@
 <?php
+use Ridibooks\Library\UrlHelper;
+use Ridibooks\Platform\Cms\Auth\AdminTagSessionOperator;
+use Ridibooks\Platform\Cms\Auth\LoginService;
+use Ridibooks\Platform\Cms\CmsApplication;
 use Ridibooks\Platform\Cms\MiniRouter;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__ . '/include/bootstrap_cms.php';
 
+// Try MiniRouter first
 $router = new MiniRouter(__DIR__ . '/controls', __DIR__ . '/views');
 $response = $router->route(Request::createFromGlobals());
-if ($response->isNotFound()) {
-	$response = new RedirectResponse('/welcome');
+if (!$response->isNotFound()) {
+	$response->send();
 }
-$response->send();
+
+
+// Try Silex Route next
+$app = new CmsApplication();
+$app['debug'] = \Config::$UNDER_DEV;
+$app['twig.path'] = [
+	__DIR__ . '/views'
+];
+
+$app->error(function (\Exception $e, $code) {
+	if ($code == 404) {
+		return new RedirectResponse('/welcome');
+	}
+
+	throw $e;
+});
+
+$app->get('/welcome', function (CmsApplication $app) {
+	return $app->render('welcome.twig');
+});
+
+$app->get('/login', function (CmsApplication $app) {
+	LoginService::resetSession();
+
+	return $app->render('login.twig');
+});
+
+$app->post('/login', function (Request $req) {
+	$id = $req->get('id');
+	$passwd = $req->get('passwd');
+	$return_url = $req->get('return_url', 'welcome');
+
+	try {
+		$login_service = new LoginService();
+		$login_service->doLoginAction($id, $passwd);
+
+		if (AdminTagSessionOperator::isPart1stCheck()) {
+			$return_url = '/admin/book/productList?type=1stCompleted';
+		} elseif (AdminTagSessionOperator::isPart2ndCheck()) {
+			$return_url = '/admin/book/productList?type=2ndCompleted';
+		} elseif (AdminTagSessionOperator::isPartMake()) {
+			$return_url = '/admin/book/productList?type=scheduled';
+		} elseif (AdminTagSessionOperator::isPartRegister()) {
+			$return_url = '/admin/book/productList?type=received';
+		} elseif (AdminTagSessionOperator::isPartPrincipal()) {
+			$return_url = '/admin/book/withholdList?type=withhold';
+		}
+
+		return RedirectResponse::create($return_url);
+	} catch (Exception $e) {
+		return UrlHelper::printAlertRedirect('/login?return_url=' . urlencode($return_url), $e->getMessage());
+	}
+});
+
+$app->get('/logout', function () {
+	LoginService::resetSession();
+	return RedirectResponse::create('/');
+});
+
+$app->run();
