bc40e155a565553cc7891a4a19d0f02421d7b904
Hyunwoo Nam <hw.nam@ridi.com>
2016-08-27 22:58:54 +0900

    [webpack] multiple entry 가능하도록 설정파일 수정

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,10 +1,14 @@
+var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './js/app.js',
+  entry: {
+    users: './js/app/users.js',
+    tags: './js/app/tags.js'
+  },
   output: {
-    path: './dist',
-    filename: 'app.bundle.js'
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
   },
   resolve: {
     modulesDirectories: ['node_modules', 'bower_components']
@@ -26,6 +30,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'commons',
+      chunks: ['users', 'tags'],
+      minChunks: 2
+    }),
     new webpack.ResolverPlugin(
       new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
     ),
