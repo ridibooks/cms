1026ed384667dd6c859b834961aed62b54474721
Hyunwoo Nam <hw.nam@ridi.com>
2016-09-19 15:38:30 +0900

    사용하지 않게된 bootstrap_cms 파일 제거

--- include/bootstrap_cms.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Deprecated use only.
-
-require_once __DIR__ . '/../../../include/config.php';
-
-// PSR-4 autoload
-$autoloader = require __DIR__ . "/../vendor/autoload.php";
-
-require_once __DIR__ . '/../bootstrap.php';
