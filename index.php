65d26675bea43d6fc639165932784f3f3fc20741
dod <bj.kim@ridi.com>
2016-09-26 16:33:08 +0900

    config 필드명 수정

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ require __DIR__ . "/vendor/autoload.php";
 
 const SESSION_TIMEOUT_SEC = 60 * 60 * 24 * 30;
 
-if (strlen(Config::$SESSION_USE_MEMCACHE)) {
-	session_set_save_handler(new CouchbaseSessionHandler(\Config::$SESSION_HOST, 'session_cms', SESSION_TIMEOUT_SEC), true);
+if (strlen(Config::$COUCHBASE_ENABLE)) {
+	session_set_save_handler(new CouchbaseSessionHandler(implode(',', \Config::$COUCHBASE_SERVER_HOSTS), 'session_cms', SESSION_TIMEOUT_SEC), true);
 }
 session_set_cookie_params(SESSION_TIMEOUT_SEC, '/', Config::$ADMIN_DOMAIN);
 session_start();
