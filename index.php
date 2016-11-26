8b056f956bfae883ee9b9cbe812830526634df8e
Hyunwoo Nam <hw.nam@ridi.com>
2016-11-26 13:31:18 +0900

    타팀에서도 자체적인 CMS 개발을 할 수 있도록 기본적인 기능들을 CmsApplication에 내장

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-use Ridibooks\Library\UrlHelper;
 use Ridibooks\Platform\Cms\Auth\LoginService;
 use Ridibooks\Platform\Cms\CmsApplication;
 use Ridibooks\Platform\Cms\MiniRouter;
 use Ridibooks\Platform\Cms\UserControllerProvider;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__ . '/../config.php';
@@ -20,40 +18,6 @@ $app = new CmsApplication();
 // Try MiniRouter first
 $app->before(function (Request $request) {
 	return MiniRouter::shouldRedirectForLogin($request);
-});
-
-$app->get('/', function () use ($app) {
-	return $app->redirect('/welcome');
-});
-
-$app->get('/welcome', function (CmsApplication $app) {
-	return $app->render('welcome.twig');
-});
-
-$app->get('/login', function (CmsApplication $app) {
-	LoginService::resetSession();
-
-	return $app->render('login.twig');
-});
-
-$app->post('/login', function (Request $req) {
-	$id = $req->get('id');
-	$passwd = $req->get('passwd');
-	$return_url = $req->get('return_url', 'welcome');
-
-	try {
-		$login_service = new LoginService();
-		$login_service->doLoginAction($id, $passwd);
-
-		return RedirectResponse::create($return_url);
-	} catch (Exception $e) {
-		return UrlHelper::printAlertRedirect('/login?return_url=' . urlencode($return_url), $e->getMessage());
-	}
-});
-
-$app->get('/logout', function () {
-	LoginService::resetSession();
-	return RedirectResponse::create('/');
 });
 
 $app->mount('/', new UserControllerProvider());
