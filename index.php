550e6b6fbecf3992ae08dffae14f7cdf62a81816
Kang Ki Tae <kt.kang@ridi.com>
2017-02-21 14:36:38 +0900

    Login with Azure oauth2.0

--- index.php
+++ index.php
@@ -13,15 +13,26 @@ if (is_readable(__DIR__ . '/../config.php')) {
 
 $autoloader = require __DIR__ . "/vendor/autoload.php";
 
+$dotenv = new Dotenv\Dotenv(__DIR__, 'config.env');
+$dotenv->load();
+
 if (isset(\Config::$COUCHBASE_ENABLE) && \Config::$COUCHBASE_ENABLE) {
 	LoginService::startCouchbaseSession(\Config::$COUCHBASE_SERVER_HOSTS);
 } else {
 	LoginService::startSession();
 }
 
-
 // Try Silex Route next
-$app = new CmsApplication();
+$app = new CmsApplication([
+	'azure' => [
+		'tenent' => $_ENV['AZURE_TENENT'],
+		'client_id' => $_ENV['AZURE_CLIENT_ID'],
+		'client_secret' => $_ENV['AZURE_CLIENT_SECRET'],
+		'resource' => $_ENV['AZURE_RESOURCE'],
+		'redirect_uri' => $_ENV['AZURE_REDIRECT_URI'],
+		'api_version' => $_ENV['AZURE_API_VERSION'],
+	],
+]);
 
 // Try MiniRouter first
 $app->before(function (Request $request) {
