f88ead1b34bcc214452151aeb2b2476caefbaa93
Jihun Kim <devgrapher@ridi.com>
2017-12-12 19:37:52 +0900

    Configure test environments

---
+++ tests/AdminUserServiceTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Ridibooks\Cms\Service;
+
+use PHPUnit\Framework\TestCase;
+use Ridibooks\Cms\Service\AdminUserService;
+
+class AdminUserServiceTest extends TestCase
+{
+
+}
