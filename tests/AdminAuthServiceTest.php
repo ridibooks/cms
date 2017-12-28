f7dbb5fe5ea553c1040de57782d3aa4a11241547
Jihun Kim <devgrapher@ridi.com>
2017-12-28 13:11:37 +0900

    메뉴 권한 Session에서 제거 (#28)
    * Remove session dependancy in AdminAuthService
    
    * Refactor hideEmptyRootMenus()
    
    * Run phpcs fix
    
    * Add type hinting
    
    * Remove redundant commets and code
    
    * Add return types & remove warnings

---
+++ tests/AdminAuthServiceTest.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace Ridibooks\Cms\Service;
+
+use PHPUnit\Framework\TestCase;
+use Ridibooks\Cms\Service\AdminAuthService;
+use Ridibooks\Cms\Service\AdminUserService;
+
+class AdminAuthServiceTest extends TestCase
+{
+    public function testCheckAuth()
+    {
+        $auth_list = ['/admin/book/productList'];
+
+        $this->assertTrue(
+            AdminAuthService::checkAuth(null, '/admin/book/productList', $auth_list)
+        );
+        $this->assertFalse(
+            AdminAuthService::checkAuth(null, '/admin/book/product', $auth_list)
+        );
+    }
+
+    public function testCheckAuth_withHash()
+    {
+        $auth_list = ['/admin/book/productList#EDIT_세트도서'];
+
+        $this->assertTrue(
+            AdminAuthService::checkAuth('EDIT_세트도서', '/admin/book/productList', $auth_list)
+        );
+        $this->assertFalse(
+            AdminAuthService::checkAuth('', '/admin/book/productList', $auth_list)
+        );
+        $this->assertFalse(
+            AdminAuthService::checkAuth('DELETE_세트도서', '/admin/book/productList', $auth_list)
+        );
+    }
+
+    public function testGetHashesFromMenus()
+    {
+        $auth_list = ['/admin/book/productList#EDIT_세트도서'];
+        $hashs = AdminAuthService::getHashesFromMenus('/admin/book/productList', $auth_list);
+        $this->assertEquals(['EDIT_세트도서'], $hashs);
+    }
+
+    public function testHideEmptyRootMenus()
+    {
+        $menus = [
+            [ 'menu_deep' => 0, 'menu_url' => '#', 'is_show' => true ],
+            [ 'menu_deep' => 0, 'menu_url' => '#', 'is_show' => true ],
+            [ 'menu_deep' => 1, 'menu_url' => '/', 'is_show' => true ],
+            [ 'menu_deep' => 0, 'menu_url' => '#', 'is_show' => true ],
+        ];
+
+        $authService = new AdminAuthService();
+        $result = $authService->hideEmptyRootMenus($menus);
+        $this->assertEquals([
+            [ 'menu_deep' => 0, 'menu_url' => '#', 'is_show' => false],
+            [ 'menu_deep' => 0, 'menu_url' => '#', 'is_show' => true],
+            [ 'menu_deep' => 1, 'menu_url' => '/', 'is_show' => true],
+            [ 'menu_deep' => 0, 'menu_url' => '#', 'is_show' => false],
+        ], $result);
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Container\Container;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+if (is_readable('.env')) {
+    $dotenv = new Dotenv\Dotenv(__DIR__, '.env');
+    $dotenv->load();
+}
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => $_ENV['MYSQL_HOST'],
+    'database' => $_ENV['MYSQL_DATABASE'],
+    'username' => $_ENV['MYSQL_USER'],
+    'password' => $_ENV['MYSQL_PASSWORD'],
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
