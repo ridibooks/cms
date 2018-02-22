b673cb9b35e61be5a8660fcb9a22955c8847acbf
Jihun Kim <devgrapher@ridi.com>
2018-02-22 19:20:15 +0900

    Set thrift endpoint in case that RPC URL is not '/'

--- web/index.php
+++ web/index.php
@@ -1,11 +1,18 @@
 <?php
 declare(strict_types=1);
 
+use Ridibooks\Cms\Thrift\ThriftService;
+
 require_once __DIR__ . "/../vendor/autoload.php";
 
 if (is_readable(__DIR__ . '/../.env')) {
     $dotenv = new Dotenv\Dotenv(__DIR__, '/../.env');
     $dotenv->overload();
+}
+
+$cms_rpc_url = $_ENV['CMS_RPC_URL'] ?? '';
+if (!empty($cms_rpc_url)) {
+    ThriftService::setEndPoint($cms_rpc_url);
 }
 
 $app = require __DIR__ . '/../src/app.php';
