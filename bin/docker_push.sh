37dd325824009c0024a1689d708d0f14821602ca
Kang Ki Tae <kt.kang@ridi.com>
2018-03-20 14:25:56 +0900

    Update CI

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-DOCKER_TAG=${TRAVIS_TAG:-latest}
-COMMIT=${TRAVIS_COMMIT::8}
+DOCKER_TAG=${DOCKER_TAG:-latest}
+DEFAULT_TAG=$(git rev-parse --short HEAD) # commit hash
 
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
-docker build -t ridibooks/cms:${COMMIT} .
 
-echo "Pushing ridibooks/cms"
-docker tag ridibooks/cms:${COMMIT} ridibooks/cms:${DOCKER_TAG}
+echo "Bulilding ridibooks/cms..."
+docker build -t ridibooks/cms:${DEFAULT_TAG} .
+echo "Builded ridibooks/cms"
+
+echo "Pushing ridibooks/cms..."
+docker tag ridibooks/cms:${DEFAULT_TAG} ridibooks/cms:${DOCKER_TAG}
 docker push ridibooks/cms
 echo "Pushed ridibooks/cms"
