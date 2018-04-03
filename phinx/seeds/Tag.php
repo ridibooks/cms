b182f51be0f6158e5a4ee9db3c7202ae0560fde8
Jihun Kim <devgrapher@ridi.com>
2018-04-03 18:07:11 +0900

    Add `getAdminTag` api (#46)

---
+++ phinx/seeds/Tag.php
@@ -0,0 +1,22 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class Tag extends AbstractSeed
+{
+    public function run()
+    {
+        $data = [
+            [
+                'name' => '관리자그룹',
+                'is_use' => 1,
+                'creator' => 'admin',
+                'reg_date' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        $posts = $this->table('tb_admin2_tag');
+        $posts->insert($data)->save();
+    }
+}
