93faafec86b676414ffb9dd03c8188c03ac9ce39
Jihun Kim <devgrapher@ridi.com>
2018-05-29 18:29:41 +0900

    Sync phinx migration with CMS-Admin

---
+++ phinx/migrations/20180523061119_add_group.php
@@ -0,0 +1,33 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddGroup extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('tb_admin2_group')
+            ->addColumn('name', 'string', ['length' => 32])
+            ->addColumn('is_use', 'boolean')
+            ->addColumn('creator', 'string', ['length' => 32])
+            ->addIndex('name')
+            ->addTimestamps()
+            ->create();
+
+        $this->table('tb_admin2_group_user')
+            ->addColumn('group_id', 'integer')
+            ->addColumn('user_id', 'string', ['length' => 32])
+            ->addForeignKey('group_id', 'tb_admin2_group', 'id') 
+            ->addForeignKey('user_id', 'tb_admin2_user', 'id') 
+            ->addIndex('user_id')
+            ->create();
+
+        $this->table('tb_admin2_group_tag')
+            ->addColumn('group_id', 'integer')
+            ->addColumn('tag_id', 'integer')
+            ->addForeignKey('group_id', 'tb_admin2_group', 'id') 
+            ->addForeignKey('tag_id', 'tb_admin2_tag', 'id') 
+            ->addIndex('tag_id')
+            ->create();
+    }
+}
